7b7f510f1b48c44ac41874515f331b50b8e6e178
Taihei Tsuchida <51879251+taiheitsuchida@users.noreply.github.com>
2021-06-27 11:32:24 +0900

    github apiに接続してlabelを取得するように変更

--- github-label-manager.py
+++ github-label-manager.py
@@ -1,7 +1,16 @@
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description='test description')
-parser.add_argument('arg1', type=str, help='test help')
+parser.add_argument('--oauth_token', type=str, help="github's oauth token.")
+parser.add_argument('--owner', type=str, help="github's oauth token.")
+parser.add_argument('--repos', type=str, help="github's oauth token.")
 args = parser.parse_args()
-print(type(args.arg1))
-print(args.arg1)
+print(type(args.oauth_token))
+print(args.oauth_token)
+
+header = {
+    'Authorization': args.oauth_token
+}
+url = f"https://api.github.com/repos/#{args.owner}/#{args.repos}/labels"
+requests.get(url, header)
